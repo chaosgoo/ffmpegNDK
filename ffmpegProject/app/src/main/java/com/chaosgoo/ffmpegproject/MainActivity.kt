ca2e12d9909f387de2e02cc94cbffc42f36256d7
chaosgoo <844268405@qq.com>
2018-02-02 16:23:27 +0800

    JNI调用Java函数

--- ffmpegProject/app/src/main/java/com/chaosgoo/ffmpegproject/MainActivity.kt
+++ ffmpegProject/app/src/main/java/com/chaosgoo/ffmpegproject/MainActivity.kt
@@ -16,15 +16,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         test_btn.setOnClickListener( View.OnClickListener {
-
+            ffmpegJni.TestFunc(1)
             var cmd = input_text.text.toString()
-            var strs = arrayOf("ffmpeg","-i","/storage/emulated/0/aaa.mp3")
+            var strs = arrayOf("ffmpeg","-codecs","-help")
+            var mp3topcm = arrayOf("ffmpeg", "-i","/storage/emulated/0/aaa.mp3", "-f", "s16be" ,"-ar" ,"44100" ,"-acodec","pcm_s16le", "/storage/emulated/0/zzz.pcm")
+            var mp3info = arrayOf("ffmpeg", "-i","/storage/emulated/0/aaa.mp3")
+            //var mp3towav = arrayOf("ffmpeg", "-i","/storage/emulated/0/aaa.mp3", "-f", "s16be" ,"-ar" ,"44100" ,"-acodec","pcm_s16le", "/storage/emulated/0/zzz.pcm")
             try {
-                f.run(strs)
+                f.run(mp3info)
             }catch (e:Exception){
                 e.printStackTrace()
             }
-
         })
     }
 }
--- ffmpegProject/app/src/main/java/com/chaosgoo/ffmpegproject/ffmpegJni.java
+++ ffmpegProject/app/src/main/java/com/chaosgoo/ffmpegproject/ffmpegJni.java
@@ -1,13 +1,29 @@
 package com.chaosgoo.ffmpegproject;
 
+import android.util.Log;
+
 /**
  * Created by Goo on 2018/2/1.
  */
 
 public class ffmpegJni {
+    int decodeTime = 0;
+    int totalDecodeTime = 0;
+    public static void onProgress(int second) {
+        Log.d("AAA", "已执行时长:" + second);
+    }
 
+    public void durationTime(int total){
+        totalDecodeTime = total;
+        Log.d("ffmpeg","asd");
+    }
+
+    public static void TestFunc(int s){
+        Log.d("FFmpeg","Called from JNI");
+    }
+    // NDK 函数
     public native int run(String[] commands);
-
+    // 导入so文件
     static {
         System.loadLibrary("avutil");
         System.loadLibrary("swresample");
