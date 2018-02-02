140d00e218f5d5dd29caa4762d6082ce17961e9d
chaosgoo <844268405@qq.com>
2018-02-02 17:55:12 +0800

    简单封装ffmpegJni类

--- ffmpegProject/app/src/main/java/com/chaosgoo/ffmpegproject/MainActivity.kt
+++ ffmpegProject/app/src/main/java/com/chaosgoo/ffmpegproject/MainActivity.kt
@@ -7,26 +7,18 @@ import android.view.View
 import android.widget.EditText
 import com.chaosgoo.ffmpegproject.R.id.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
-    var f: ffmpegJni = ffmpegJni()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        var f: ffmpegJni = ffmpegJni("/storage/emulated/0/aaa.mp3","/storage/emulated/0/zzz.pcm",progressBar)
+        f!!.init()
         test_btn.setOnClickListener( View.OnClickListener {
-            ffmpegJni.TestFunc(1)
-            var cmd = input_text.text.toString()
-            var strs = arrayOf("ffmpeg","-codecs","-help")
-            var mp3topcm = arrayOf("ffmpeg", "-i","/storage/emulated/0/aaa.mp3", "-f", "s16be" ,"-ar" ,"44100" ,"-acodec","pcm_s16le", "/storage/emulated/0/zzz.pcm")
-            var mp3info = arrayOf("ffmpeg", "-i","/storage/emulated/0/aaa.mp3")
-            //var mp3towav = arrayOf("ffmpeg", "-i","/storage/emulated/0/aaa.mp3", "-f", "s16be" ,"-ar" ,"44100" ,"-acodec","pcm_s16le", "/storage/emulated/0/zzz.pcm")
-            try {
-                f.run(mp3info)
-            }catch (e:Exception){
-                e.printStackTrace()
-            }
+              f!!.decode()
         })
     }
 }
--- ffmpegProject/app/src/main/java/com/chaosgoo/ffmpegproject/ffmpegJni.java
+++ ffmpegProject/app/src/main/java/com/chaosgoo/ffmpegproject/ffmpegJni.java
@@ -1,28 +1,58 @@
 package com.chaosgoo.ffmpegproject;
 
+import android.media.MediaPlayer;
 import android.util.Log;
+import android.widget.ProgressBar;
 
 /**
  * Created by Goo on 2018/2/1.
  */
 
 public class ffmpegJni {
-    int decodeTime = 0;
-    int totalDecodeTime = 0;
-    public static void onProgress(int second) {
-        Log.d("AAA", "已执行时长:" + second);
+    private  float totalDecodeTime = 349.0f;
+    private  ProgressBar progressBar;
+
+    private String fileInPath;
+    private String fileOutPath;
+
+    MediaPlayer mediaPlayer = new MediaPlayer();
+
+    public ffmpegJni(String inPath, String outPath, ProgressBar progressBar){
+        fileInPath = inPath;
+        fileOutPath = outPath;
+        this.progressBar = progressBar;
+    }
+    // 初始化相关设定
+    public void init(){
+        try{
+            mediaPlayer.setDataSource(fileInPath);
+            mediaPlayer.prepare();
+            totalDecodeTime = mediaPlayer.getDuration()/1000;
+            Log.d("ffmpeg", "init: "+totalDecodeTime);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    //执行解码操作
+    public void decode(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String[] cmd = {"ffmpeg", "-i",fileInPath, "-f", "s16be" ,"-ar" ,"44100" ,"-acodec","pcm_s16le", fileOutPath};
+                main(cmd);
+            }
+        }).start();
     }
 
-    public void durationTime(int total){
-        totalDecodeTime = total;
-        Log.d("ffmpeg","asd");
+    // 更新进度条
+    public void onProgress(int second) {
+        Log.d("ffmpeg", "onProgress: "+ ((second/this.totalDecodeTime) * 100));
+        progressBar.setProgress((int)((second/this.totalDecodeTime) * 100));
     }
 
-    public static void TestFunc(int s){
-        Log.d("FFmpeg","Called from JNI");
-    }
+
     // NDK 函数
-    public native int run(String[] commands);
+    public native int main(String[] commands);
     // 导入so文件
     static {
         System.loadLibrary("avutil");
